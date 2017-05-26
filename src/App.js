691268fd344957df3ae8605e193ba268f33928a0
Tomasz Lakomy <tomasz.lakomy@grandparade.co.uk>
2017-05-26 11:24:13 +0200

    Reset main App.js file to 'Hello World'

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {items: [], filter: ''}
-    }
-
-    componentDidMount(){
-        fetch( 'https://swapi.co/api/people/?format=json' )
-            .then( response => response.json() )
-            .then( ({results: items}) => this.setState({items}))
-    }
-
-    setNewFilter(e){
-        this.setState({filter: e.target.value})
-    }
-
-    filterItems() {
-        let items = this.state.items;
-        if(this.state.filter){
-            items = items.filter(item => {
-                return item.name.toLowerCase().includes(this.state.filter.toLowerCase())
-            })
-        }
-        return items;
-    }
-
-    render(){
+    render() {
         return (
-            <div>
-                <input
-                    type="text"
-                    onChange={this.setNewFilter.bind(this)}
-                />
-                {this.filterItems().map(item =>
-                    <Person key={item.name} person={item} />)}
-            </div>
-        )
+            <h1>Hello World!</h1>
+        );
     }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
-
-export default App
+export default App;
